6eae850abe59564b66379b21facc7e5b3aa61a75
Vikil Naik <vikilnaik37@gmail.com>
2019-03-28 13:55:07 -0400

    created a button to get the ajax requests

--- first_ajax/static/js/main.js
+++ first_ajax/static/js/main.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
-  
+
+    const getDataBtn = document.getElementById("data_button");
+    console.log('Get more data', getDataBtn);
+
+
+    getDataBtn.addEventListener('click', function() {
+        console.log("Button Click 😃")
+        const request = axios.get('http://intro-ajax-api.herokuapp.com/')
+            .then(function(resp) {
+                const dataElem = document.createElement('div');
+                dataElem.innerHTML = resp.data;
+                document.body.appendChild(dataElem);
+
+
+            });
+
+    });
+
 });
